4f701fcdcc96a4028099508615044be4c26b6675
gines <ginesgasz@gmail.com>
2022-09-25 15:22:26 -0300

    Message fetch now starts from last message timestamp

--- services/fetchMessages.js
+++ services/fetchMessages.js
@@ -1,15 +1,41 @@
+const knex = require('knex')({
+	client: 'sqlite3',
+	connection: {
+		filename: './discord.db',
+	},
+	useNullAsDefault: true,
+});
+
 const fetchMessages = async (channel) => {
+	const options = { limit: 100 };
 	let sumMessages = [];
 	let messages;
 	let messagePointer;
+	const messagesExists = await knex.schema.hasTable('messages');
+	if (messagesExists) {
+		const lastMessageDb = await knex
+			.from('messages')
+			.select(knex.raw('CAST (message_id AS CHAR) AS message_id'))
+			.where({
+				channel_id: channel.id,
+			})
+			.max('created_at');
+		messagePointer = lastMessageDb[0]?.message_id;
+	}
+	const isAfter = !!messagePointer;
+
+	const setOptions = () => {
+		isAfter ? options.after = messagePointer : options.before = messagePointer;
+	};
+
 	do {
-		const options = { limit: 100 };
-		if (messagePointer) { options.before = messagePointer; }
+		setOptions();
 		messages = await channel.messages.fetch(options);
 		sumMessages = sumMessages.concat(Array.from(messages.values()));
-		messagePointer = messages.last().id;
-		console.log(sumMessages.length, "messages counted");
-	} while ((messages.size === 100) && (sumMessages.length < 400));
+		messagePointer = isAfter ? messages.first().id : messages.last().id;
+		console.log(sumMessages.length, 'messages counted');
+	} while (messages.size === 100);
+
 	return sumMessages;
 
 };
