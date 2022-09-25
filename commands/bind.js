97a20d508ec6b71020d13539b82d17f3592a5d44
gines <ginesgasz@gmail.com>
2022-09-25 15:26:27 -0300

    New bind command adds the current channel from command to bound_channels, also triggers message fetch on the channel

---
+++ commands/bind.js
@@ -0,0 +1,33 @@
+const { SlashCommandBuilder } = require('discord.js');
+const { getMessagesFromChannel } = require('../services/getMessagesFromChannel');
+const knex = require('knex')({
+	client: 'sqlite3',
+	connection: {
+		filename: './discord.db',
+	},
+	useNullAsDefault: true,
+});
+
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('bind')
+		.setDescription('binds bot to track this channel and stores previous messages'),
+	async execute(interaction) {
+		await interaction.deferReply();
+		const boundChannelsExists = await knex.schema.hasTable('bound_channels');
+		if (!boundChannelsExists) {
+			await knex.schema.createTable('bound_channels', table => {
+				table.integer('channel_id').primary().notNullable();
+				table.text('guild_id').notNullable();
+			});
+		}
+
+		await knex('bound_channels').insert({
+			channel_id: interaction.channelId,
+			guild_id: interaction.guildId,
+		});
+		await getMessagesFromChannel(interaction.channelId, interaction.client);
+		await interaction.editReply('successfully bound bot to this channel');
+	},
+};
