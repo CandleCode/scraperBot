9f909c8aebb9b27ee3689913d734c8a16d2331a1
gines <ginesgasz@gmail.com>
2022-09-28 14:02:26 -0300

    test command- ignore

--- commands/ping.js
+++ commands/ping.js
@@ -1,10 +1,12 @@
 const { SlashCommandBuilder } = require('discord.js');
+const { getMessagesFromBoundChannels } = require('../services/getMessagesFromBoundChannels');
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('ping')
 		.setDescription('Replies with Pong!'),
 	async execute(interaction) {
+		await getMessagesFromBoundChannels(interaction.client);
 		await interaction.reply('Pong!');
 	},
 };
